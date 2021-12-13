c4c7af2346e935efff8f3b2905676214a68bba1d
ZhiJuanTang <tangzhijuan56@gmail.com>
2021-12-13 16:19:30 +0100

    API fethed

--- src/Generator.js
+++ src/Generator.js
@@ -1,32 +1,69 @@
-import { useState } from "react";
-
-const phrases = [
-	"diwali greetings message 1",
-	"diwali greetings message 2",
-	"diwali greetings message 3",
-];
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Generator() {
-	const [msg, setMsg] = useState("");
-	const [bg, setBg] = useState();
+  const [msg, setMsg] = useState([]);
+  const [images, setImages] = useState();
+  const [loading, setLoading] = useState(false);
+  const [id, setId] = useState(1);
 
-	function generate() {
-		const phrase = phrases[Math.floor(Math.random() * 3)];
-		const newBg = Math.floor(Math.random() * 3 + 1);
-		setMsg(phrase);
-		setBg(newBg);
-	}
-	console.log(bg);
-	return (
-		<>
-			<div className={`bgGlobal bgChooser${bg}`}>
-				<p>
-					<h2>Happy Diwali!</h2>
-					<br />
-					{msg}
-				</p>
-			</div>
-			<button onClick={generate}>GENERATE</button>
-		</>
-	);
+  useEffect(() => {
+    const getImages = async () => {
+      try {
+        setLoading(true);
+        const { data: [image] } = await axios.get(
+          `https://cardgeneratordiwali.herokuapp.com/images/${id}`
+        );
+        setImages(image);
+        setLoading(false);
+      } catch (error) {
+        return alert("Sorry, no images");
+      }
+    };
+    getImages();
+  }, [id]);
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        setLoading(true);
+        const { data: [message] } = await axios.get(
+          `https://cardgeneratordiwali.herokuapp.com/messages/${id}`
+        );
+        setMsg(message);
+
+        setLoading(false);
+      } catch (error) {
+        return alert("Sorry, no msgs");
+      }
+    };
+    getMessages();
+  }, [id]);
+
+  console.log(msg);
+
+  function generate() {
+	setId(Math.floor(Math.random() * 10) + 1);
+    // if (msg) {
+    //   const phrase = msg[Math.floor(Math.random() * 3)];
+    //   const newBg = Math.floor(Math.random() * 3 + 1);
+    //   console.log("phrase", phrase);
+    //   setMsg(phrase);
+    //   setImages(newBg);
+    }
+  
+  console.log(images);
+  return (
+    <>
+      <div className={`bgGlobal bgChooser${images}`}>
+        <p>
+          {msg && msg.title}
+          {msg && msg.text}
+          {/* you might want to use the images.url property as a src attribute in an img tag */}
+          {images && <img  src={images.url} />}
+        </p>
+      </div>
+      <button onClick={generate}>GENERATE</button>
+    </>
+  );
 }
