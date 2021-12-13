bd41390f1e328d62383f7c49d7c497125dfa4ffc
Francine Melo Pêpe <franmelopepe2@gmail.com>
2021-12-13 12:32:41 +0100

    basic layout (still working on it)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import "./App.css";
+import "./App.css"; 
+
+
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<p>Diwali Greetings Generator</p>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+			
+          	<div className="header_box">
+			  <div className="header_box_logo">
+
+			  </div>
+            
+            {/* <p>Diwali Greetings Generator</p> */}
+          </div>
+        </header>
+      </div>
+    </div>
+  );
 }
 
 export default App;
