04987b887e980766c5fbccf524e16c752e66b605
Staś Werno <89650713+staswerno@users.noreply.github.com>
2021-12-13 15:37:03 +0100

    add local data and styling

--- src/Generator.js
+++ src/Generator.js
@@ -1,18 +1,21 @@
 import { useState } from "react";
 
 const phrases = [
-	"first diwali greetings message first diwali greetings message first diwali greetings message first diwali greetings message ",
-	"second diwali greetings message second diwali greetings message second diwali greetings message second diwali greetings message ",
-	"third diwali greetings message third diwali greetings message third diwali greetings message third diwali greetings message ",
+	"Wishing the goodness of this festive season dwells within you and stays throughout the year.",
+	"Hoping this Diwali turns out to be an extra special one for you and your dear ones and may success be always with you.",
+	"May the beauty of the festival of lights fill your home with happiness and my the new year bring joy, peace, and prosperity in your life.",
+	"May the festival of joy become more beautiful for you and family. All your new ventures get success and progress.",
 ];
 
 export default function Generator() {
-	const [msg, setMsg] = useState("diwali greetings message 1");
+	const [msg, setMsg] = useState(
+		"May the festival of joy become more beautiful for you and family. All your new ventures get success and progress."
+	);
 	const [bg, setBg] = useState(1);
 
 	function generate() {
-		const phrase = phrases[Math.floor(Math.random() * 3)];
-		const newBg = Math.floor(Math.random() * 3 + 1);
+		const phrase = phrases[Math.floor(Math.random() * 4)];
+		const newBg = Math.floor(Math.random() * 6 + 1);
 		setMsg(phrase);
 		setBg(newBg);
 	}
